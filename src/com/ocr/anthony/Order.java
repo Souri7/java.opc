9a4fd5538baac802f6e3d30d179aa22b028a146b
Ludivine Rousseau <ludivine.rousseau@outlook.fr>
2019-09-16 16:05:46 +0200

    ajout des tests ainsi que de l'affichage et de la fonction du choix de l'accompagnement

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -3,6 +3,7 @@ package com.ocr.anthony;
 import java.util.Scanner;
 
 public class Order {
+	public boolean allSidesEnable;
     /**
      * Display all available menus in the restaurant.
      */
@@ -34,6 +35,68 @@ public class Order {
         }
     }
     /**
+     * Display all available side for the menu in the restaurant.
+     */
+    public void displayAvailableSide(int nbMenu) {
+    	
+
+    	System.out.println("Choix accompagnement");
+    	if (nbMenu==1||nbMenu==2) {
+    	allSidesEnable = true;
+        System.out.println("1 - légumes frais");
+        System.out.println("2 - frites");
+        System.out.println("3 - riz");
+        
+        };
+        if(nbMenu==3) {
+        System.out.println("1 - riz");
+        System.out.println("2 - pas de riz");
+        }
+        System.out.println("Que souhaitez-vous comme accompagnement ?");
+
+        }
+    
+    /**
+     * Display a selected side depending on all sides enable or not.
+     * All sides = vegetables, chips and rice
+     * No all sides = rice or not
+     * @param nbSide The selected Side
+     * @param allSidesEnable  enable display for all side or not
+     */
+    public void displaySelectedSide(int nbSide, boolean allSidesEnable) {
+
+    	    if (allSidesEnable) {
+    	        switch (nbSide) {
+    	            case 1:
+    	                System.out.println("Vous avez choisi comme accompagnement : légumes frais");
+    	                break;
+    	            case 2:
+    	                System.out.println("Vous avez choisi comme accompagnement : frites");
+    	                break;
+    	            case 3:
+    	                System.out.println("Vous avez choisi comme accompagnement : riz");
+    	                break;
+    	            default:
+    	                System.out.println("Vous n'avez pas choisi d'accompagnement parmi les choix proposés");
+    	                break;
+    	        }
+    	    } else {
+    	        switch (nbSide) {
+    	            case 1:
+    	                System.out.println("Vous avez choisi comme accompagnement : riz");
+    	                break;
+    	            case 2:
+    	                System.out.println("Vous avez choisi comme accompagnement : pas de riz");
+    	                break;
+    	            default:
+    	                System.out.println("Vous n'avez pas choisi d'accompagnement parmi les choix proposés");
+    	                break;
+    	        }
+    	    }
+    	}
+    
+    
+    /**
      * Run asking process for a menu.
      */
     public void runMenu() {
@@ -41,5 +104,9 @@ public class Order {
         Scanner sc = new Scanner(System.in);
         int nb = sc.nextInt();
         this.displaySelectedMenu(nb);
+        this.displayAvailableSide(nb);
+        Scanner sc1 = new Scanner(System.in);
+        int nbSide = sc1.nextInt();
+        this.displaySelectedSide(nbSide, allSidesEnable);
     }
-    }
+}
