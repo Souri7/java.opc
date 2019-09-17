b1e248b5edf9c53edd75ca875cdb8b56a69dfc03
Ludivine Rousseau <ludivine.rousseau@outlook.fr>
2019-09-17 10:23:54 +0200

    création de la méthode askSomething, qui servira à afficher n'importe quelle question

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -180,4 +180,31 @@ public class Order {
             runMenu();
         }
     }
+	/**
+ * Display a question about a category in the standard input, get response and display it
+ * @param category the category of the question
+ * @param responses available responses
+ */
+public void askSomething(String category, String[] responses) {
+    System.out.println("Choix " + category);
+    for (int i = 1; i <= responses.length; i++) {
+        System.out.println(i + " - " + responses[i - 1]);
+    }
+    System.out.println("Que souhaitez vous comme " + category + "?");
+    int nbResponse;
+    boolean responseIsGood;
+    do {
+        nbResponse =sc.nextInt();
+        if (nbResponse >= 1 && nbResponse <= responses.length) {
+            responseIsGood = true;
+        } else {
+            responseIsGood = false;
+        }
+        if (responseIsGood == true) {
+            System.out.println("Vous avez choisi comme " + category + " : " + responses[nbResponse - 1]);
+        } else {
+            System.out.println("Vous n'avez pas choisi de " + category + " parmi les choix proposés");
+        }
+    } while (responseIsGood == false);
+}
     }
