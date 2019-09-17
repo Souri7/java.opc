7f04aba35c194cac84a4613f4766c3cc19a4d214
Ludivine Rousseau <ludivine.rousseau@outlook.fr>
2019-09-17 10:41:21 +0200

    optimisation du codede askSomething pour qu'il soit plus lisible

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -165,30 +165,24 @@ public class Order {
      */
     public void askSomething(String category, String[] responses) {
         System.out.println("Choix " + category);
-        for (int i = 1; i <= responses.length; i++) {
+        for (int i = 1; i <= responses.length; i++)
             System.out.println(i + " - " + responses[i - 1]);
-        }
-        System.out.println("Que souhaitez vous comme " + category + "?");
+        System.out.println("Que souhaitez-vous comme " + category + "?");
         int nbResponse;
         boolean responseIsGood;
         do {
             nbResponse = sc.nextInt();
-            if (nbResponse >= 1 && nbResponse <= responses.length) {
-                responseIsGood = true;
-            } else {
-                responseIsGood = false;
-            }
-            if (responseIsGood == true) {
+            responseIsGood = (nbResponse >= 1 && nbResponse <= responses.length);
+            if (responseIsGood)
                 System.out.println("Vous avez choisi comme " + category + " : " + responses[nbResponse - 1]);
-            } else {
+            else {
                 boolean isVowel = "aeiouy".contains(Character.toString(category.charAt(0)));
-                if (isVowel) {
+                if (isVowel)
                     System.out.println("Vous n'avez pas choisi d'" + category + " parmi les choix proposés");
-                } else {
+                else
                     System.out.println("Vous n'avez pas choisi de " + category + " parmi les choix proposés");
-                }
             }
-        } while (responseIsGood == false);
+        } while (!responseIsGood);
     }
     /**
      * Display a question about menu in the standard input, get response and display it
