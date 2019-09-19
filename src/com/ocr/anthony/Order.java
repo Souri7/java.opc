b0a0b7df39fac220ef653560cb3bc25a8d97b6b2
Ludivine Rousseau <ludivine.rousseau@outlook.fr>
2019-09-19 13:53:53 +0200

    ajout de la lecture/ecriture de fichier csv pour gérer la transmission des menus

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -1,7 +1,14 @@
 package com.ocr.anthony;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+
+import static java.nio.file.StandardOpenOption.APPEND;
+
+import java.io.IOException;
 
 public class Order {
     Scanner sc = new Scanner(System.in);
@@ -40,26 +47,30 @@ public class Order {
     /**
      * Run asking process for a menu.
      */
-    public void runMenu() {
+    public String runMenu() {
         int nbMenu = askMenu();
+        int nbSide = -1;
+        int nbDrink = -1;
         switch (nbMenu) {
             case 1:
-                askSide(true);
-                askDrink();
+                nbSide = askSide(true);
+                nbDrink = askDrink();
                 break;
             case 2:
-                askSide(true);
+                nbSide = askSide(true);
                 break;
             case 3:
-                askSide(false);
-                askDrink();
+                nbSide = askSide(false);
+                nbDrink = askDrink();
                 break;
         }
+        return nbMenu+","+ nbSide+","+nbDrink+"%n";
     }
     /**
      * Run asking process for several menus.
      */
     public void runMenus() {
+        Path orderPath = Paths.get("order.csv");
         System.out.println("Combien souhaitez vous commander de menu ?");
         int menuQuantity = -1;
         boolean responseIsGood;
@@ -76,7 +87,13 @@ public class Order {
         orderSummary = "Résumé de votre commande :%n";
         for (int i = 0; i < menuQuantity; i++) {
             orderSummary += "Menu " + (i + 1) + ":%n";
-            runMenu();
+            String orderLine = runMenu();
+            try {
+				Files.write(orderPath, String.format(orderLine).getBytes(), APPEND);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+			    System.out.println("Ooops une erreur est survenue. Merci de réessayer plus tard");
+			}
         }
         System.out.println("");
         System.out.println(String.format(orderSummary));
@@ -216,21 +233,21 @@ public class Order {
     /**
      * Display a question about side in the standard input, get response and display it
      */
-    public void askSide(boolean allSidesEnable) {
+    public int askSide(boolean allSidesEnable) {
         if (allSidesEnable) {
             String[] responsesAllSide = {"lÃ©gumes frais", "frites", "riz"};
-            askSomething("accompagnement", responsesAllSide);
+            return askSomething("accompagnement", responsesAllSide);
         } else {
             String[] responsesOnlyRice = {"riz", "pas de riz"};
-            askSomething("accompagnement", responsesOnlyRice);
+            return askSomething("accompagnement", responsesOnlyRice);
         }
     }
 
     /**
      * Display a question about drink in the standard input, get response and display it
      */
-    public void askDrink() {
+    public int askDrink() {
         String[] responsesDrink = {"eau plate", "eau gazeuse", "soda"};
-        askSomething("boisson", responsesDrink);
+        return askSomething("boisson", responsesDrink);
     }
 }
